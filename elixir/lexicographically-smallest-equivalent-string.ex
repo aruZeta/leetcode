6914aa4ef97cd343e325b42ad5c7d36e0a3fa1e8
aruZeta <aru_hackZ.official@zohomail.eu>
2023-01-15 10:47:59 +0100

    [fix] Missing Topics section

--- elixir/lexicographically-smallest-equivalent-string.ex
+++ elixir/lexicographically-smallest-equivalent-string.ex
@@ -1,3 +1,5 @@
+# Topics: String, Union Find
+
 defmodule Solution do
   @spec smallest_equivalent_string(s1 :: String.t, s2 :: String.t, base_str :: String.t) :: String.t
   def smallest_equivalent_string(s1, s2, base_str) do
